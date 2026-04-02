b313495c8a023e85187980feea2b83a243dab5f6
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-04-02 18:56:32 +0000

    chore: add settings configuration using Pydantic Settings

---
+++ app/core/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str | None = None
+    model_config = SettingsConfigDict(case_sensitive=True, env_file='.env')
+
+
+settings = Settings()
