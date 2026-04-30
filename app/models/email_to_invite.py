be73801dfc8693a8310c72b27bf4ad884d2e1669
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-04-30 01:49:58 +0000

    refactor: add UniqueConstraint for email and trip_id in EmailToInvite model

--- app/models/email_to_invite.py
+++ app/models/email_to_invite.py
@@ -1,6 +1,6 @@
 from app.db.base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, UniqueConstraint
 from typing import TYPE_CHECKING
 import uuid
 
@@ -10,9 +10,10 @@ if TYPE_CHECKING:
 
 class EmailToInvite(Base):
     __tablename__ = 'emails_to_invite'
+    __table_args__ = UniqueConstraint('email', 'trip_id')
 
     email: Mapped[str] = mapped_column(
-        String(200), nullable=False, unique=True)
+        String(200), nullable=False)
     fullname: Mapped[str] = mapped_column(String(200), nullable=True)
     presence: Mapped[bool] = mapped_column(
         Boolean, nullable=False, default=False)
