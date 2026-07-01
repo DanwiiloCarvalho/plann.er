5dfbdeb499f94c4584242c8ff73c753be7b4d739
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-01 19:02:58 +0000

    refactor: update LinkRepository to utilize LinkMapper for domain conversion and improve method naming

--- app/adapters/outbound/database/repositories/link_repository.py
+++ app/adapters/outbound/database/repositories/link_repository.py
@@ -1,20 +1,21 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from sqlalchemy.exc import SQLAlchemyError
 from collections.abc import Sequence
+from app.adapters.outbound.database.mappers.link_mapper import LinkMapper
 from app.adapters.outbound.database.repositories.base_repository import BaseRepository
-from app.adapters.outbound.database.models.link import Link
+from app.adapters.outbound.database.models.link import Link as LinkModel
+from app.domain.entities.link import Link as LinkDomain
 from uuid import UUID
 
 
-class LinkRepository(BaseRepository):
+class LinkRepository(BaseRepository[LinkModel, LinkDomain]):
     def __init__(self, db_session: AsyncSession) -> None:
-        super().__init__(db_session, Link)
+        super().__init__(db_session, LinkModel, LinkMapper)
 
-    async def find_links_from_trip(self, trip_id: UUID) -> list[Link]:
-        try:
-            query = select(Link).filter(Link.trip_id == trip_id)
-            links_found: Sequence[Link] = (await self._db_session.execute(query)).scalars().all()
-            return list(links_found)
-        except SQLAlchemyError as e:
-            raise e
+    async def find_links_from_trip_id(self, trip_id: UUID) -> list[LinkDomain]:
+        query = select(self._model_cls).filter(
+            self._model_cls.trip_id == trip_id)
+        result = await self._db_session.execute(query)
+        links_found: Sequence[LinkModel] = result.scalars().all()
+
+        return [self._mapper.to_domain(link) for link in list(links_found)]
