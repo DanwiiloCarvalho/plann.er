0b5210c1648d634841c0469a150c4769bad01a9a
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-03 14:08:36 +0000

    refactor: update repositories to use mappers for domain conversion and improve method naming

--- app/adapters/outbound/database/repositories/email_to_invite_repository.py
+++ app/adapters/outbound/database/repositories/email_to_invite_repository.py
@@ -3,49 +3,59 @@ from sqlalchemy import select
 from collections.abc import Sequence
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.exc import SQLAlchemyError
+from app.adapters.outbound.database.mappers.email_to_invite_mapper import EmailToInviteMapper
 from app.adapters.outbound.database.repositories.base_repository import BaseRepository
-from app.adapters.outbound.database.models.email_to_invite import EmailToInvite
+from app.adapters.outbound.database.models.email_to_invite import EmailToInvite as EmailToInviteModel
+from app.domain.entities.email_to_invite import EmailToInvite as EmailToInviteDomain
 from uuid import UUID
 
 
-class EmailToInviteRepository(BaseRepository):
+class EmailToInviteRepository(BaseRepository[EmailToInviteModel, EmailToInviteDomain]):
     def __init__(self, db_session: AsyncSession) -> None:
-        super().__init__(db_session, EmailToInvite)
+        super().__init__(db_session, EmailToInviteModel, EmailToInviteMapper)
 
-    async def confirm_presence_by_id(self, email_to_invite_id: UUID) -> EmailToInvite | None:
-        try:
-            query = select(EmailToInvite).filter(
-                EmailToInvite.id == email_to_invite_id)
-            email_to_invite_found: EmailToInvite | None = (await self._db_session.execute(query)).scalar_one_or_none()
+    async def confirm_presence_by_id(self, email_to_invite_id: UUID) -> EmailToInviteDomain:
+        query = select(self._model_cls).filter(
+            self._model_cls.id == email_to_invite_id)
+        result = await self._db_session.execute(query)
+        email_to_invite_found: EmailToInviteModel | None = result.scalar_one_or_none()
 
-            if not email_to_invite_found:
-                return None
+        if not email_to_invite_found:
+            raise ValueError(
+                f'Email to invite with id {email_to_invite_id} not found')
 
-            email_to_invite_found.presence = True
-            await self._db_session.commit()
-            return email_to_invite_found
-        except SQLAlchemyError as e:
-            await self._db_session.rollback()
-            raise e
+        email_to_invite_found.presence = True
+        return self._mapper.to_domain(email_to_invite_found)
 
-    async def add_emails_bulk(self, emails_to_invite: list[EmailToInvite]) -> list[EmailToInvite]:
+        # query = select(EmailToInvite).filter(
+        #     EmailToInvite.id == email_to_invite_id)
+        # email_to_invite_found: EmailToInvite | None = (await self._db_session.execute(query)).scalar_one_or_none()
 
-        if not emails_to_invite:
-            return []
+        # if not email_to_invite_found:
+        #     return None
 
-        try:
-            self._db_session.add_all(emails_to_invite)
-            await self._db_session.commit()
-            return emails_to_invite
-        except IntegrityError as e:
-            await self._db_session.rollback()
-            raise e
+        # email_to_invite_found.presence = True
+        # await self._db_session.commit()
+        # return email_to_invite_found
 
-    async def find_emails_from_trip(self, trip_id: UUID) -> list[EmailToInvite]:
-        try:
-            query = select(EmailToInvite).filter(
-                EmailToInvite.trip_id == trip_id)
-            emails_found: Sequence[EmailToInvite] = (await self._db_session.execute(query)).scalars().all()
-            return list(emails_found)
-        except SQLAlchemyError as e:
-            raise e
+    # async def add_emails_bulk(self, emails_to_invite: list[EmailToInvite]) -> list[EmailToInvite]:
+
+    #     if not emails_to_invite:
+    #         return []
+
+    #     try:
+    #         self._db_session.add_all(emails_to_invite)
+    #         await self._db_session.commit()
+    #         return emails_to_invite
+    #     except IntegrityError as e:
+    #         await self._db_session.rollback()
+    #         raise e
+
+    # async def find_emails_from_trip(self, trip_id: UUID) -> list[EmailToInvite]:
+    #     try:
+    #         query = select(EmailToInvite).filter(
+    #             EmailToInvite.trip_id == trip_id)
+    #         emails_found: Sequence[EmailToInvite] = (await self._db_session.execute(query)).scalars().all()
+    #         return list(emails_found)
+    #     except SQLAlchemyError as e:
+    #         raise e
--- app/adapters/outbound/database/repositories/trip_repository.py
+++ app/adapters/outbound/database/repositories/trip_repository.py
@@ -1,37 +1,33 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from collections.abc import Sequence
-from app.adapters.outbound.database.models.trip import Trip
+from app.adapters.outbound.database.mappers.trip_mapper import TripMapper
+from app.adapters.outbound.database.models.trip import Trip as TripModel
 from app.adapters.outbound.database.repositories.base_repository import BaseRepository
 from uuid import UUID
-from sqlalchemy.exc import SQLAlchemyError
+
+from app.domain.entities.trip import Trip as TripDomain
+from app.domain.exceptions.entity_not_found import EntityNotFoundError
 
 
-class TripRepository(BaseRepository):
+class TripRepository(BaseRepository[TripModel, TripDomain]):
     def __init__(self, db_session: AsyncSession) -> None:
-        super().__init__(db_session, Trip)
+        super().__init__(db_session, TripModel, TripMapper)
 
-    async def get_by_owner_name(self, owner_name: str) -> list[Trip]:
-        try:
-            query = select(Trip).filter(Trip.owner_name == owner_name)
-            result = await self._db_session.execute(query)
-            trip_founded: Sequence[Trip] = result.unique().scalars().all()
-            return list(trip_founded)
-        except SQLAlchemyError as e:
-            raise e
+    async def get_trips_by_owner_name(self, owner_name: str) -> list[TripDomain]:
+        query = select(self._model_cls).filter(
+            self._model_cls.owner_name == owner_name)
+        result = await self._db_session.execute(query)
+        trips_founded: Sequence[TripModel] = result.unique().scalars().all()
+        return [self._mapper.to_domain(trip) for trip in trips_founded]
 
-    async def confirm_trip(self, trip_id: UUID) -> Trip | None:
-        try:
-            query = select(Trip).filter(Trip.id == trip_id)
-            result = await self._db_session.execute(query)
-            trip_found: Trip | None = result.unique().scalar_one_or_none()
+    async def confirm_trip(self, trip_id: UUID) -> TripDomain:
+        query = select(self._model_cls).filter(self._model_cls.id == trip_id)
+        result = await self._db_session.execute(query)
+        trip_found: TripModel | None = result.scalar_one_or_none()
 
-            if not trip_found:
-                return None
+        if not trip_found:
+            raise EntityNotFoundError(trip_id)
 
-            trip_found.status = True
-            await self._db_session.commit()
-            return trip_found
-        except SQLAlchemyError as e:
-            await self._db_session.rollback()
-            raise e
+        trip_found.status = True
+        return self._mapper.to_domain(trip_found)
