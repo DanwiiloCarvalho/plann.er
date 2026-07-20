15bc1a8e325be3d3c3b009ca4d72979a7cc4bb81
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-20 22:14:17 +0000

    fix: change email type in EmailToInviteDTO from str_schema to str for improved clarity

--- app/application/dto/email_dto.py
+++ app/application/dto/email_dto.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
 
-from pydantic_core.core_schema import str_schema
-
 
 @dataclass
 class EmailToInviteDTO:
     fullname: str
-    email: str_schema
+    email: str
