0f72fe64ae777d85ab615496fceff855562ab567
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-21 13:54:52 +0000

    feat: add CreateTripOutputDTO for trip creation response with unique identifier and trip details

--- app/application/dto/trip_dto.py
+++ app/application/dto/trip_dto.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import date
+import uuid
 
 from app.application.dto.email_dto import EmailToInviteDTO
 
@@ -12,3 +13,13 @@ class CreateTripDTO:
     owner_name: str
     owner_email: str
     emails_to_invite: list[EmailToInviteDTO]
+
+
+@dataclass
+class CreateTripOutputDTO:
+    id: uuid.UUID
+    destination: str
+    start_date: date
+    end_date: date
+    owner_name: str
+    owner_email: str
