621ebebcedf5309958048fc4410aa90346aa3a80
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-21 20:40:05 +0000

    feat: add GetTripResponse schema for trip details and invite management

---
+++ app/adapters/inbound/api/schemas/get_trip_response.py
@@ -0,0 +1,27 @@
+from datetime import date
+import uuid
+
+from pydantic import EmailStr
+from app.adapters.inbound.api.schemas.base_schema import BaseSchema
+
+
+class GetEmailToInvite(BaseSchema):
+    fullname: str
+    email: EmailStr
+    presence: bool
+
+
+class GetLink(BaseSchema):
+    link: str
+    title: str
+
+
+class GetTripResponse(BaseSchema):
+    id: uuid.UUID
+    destination: str
+    start_date: date
+    end_date: date
+    owner_name: str
+    owner_email: EmailStr
+    emails_to_invite: list[GetEmailToInvite]
+    links: list[GetLink]
