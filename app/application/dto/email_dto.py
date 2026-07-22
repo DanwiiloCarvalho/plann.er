91c065383ca7061f737e622355e853f6e5eb28f8
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-22 23:42:49 +0000

    feat: add EmailToInviteResponseDTO for handling email invitation responses, including ID and presence status

--- app/application/dto/email_dto.py
+++ app/application/dto/email_dto.py
@@ -1,10 +1,19 @@
 from dataclasses import dataclass
+import uuid
 
 
 @dataclass
 class EmailToInviteDTO:
     fullname: str
     email: str
+
+
+@dataclass
+class EmailToInviteResponseDTO:
+    id: uuid.UUID
+    fullname: str
+    email: str
+    presence: bool
 
 
 @dataclass
