d4831e0e2b9e4b66aaee71c8bee9e102fce095ac
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-22 02:47:46 +0000

    feat: implement CreateTripLinkUseCase to manage link creation for trips, including error handling for non-existent trips

---
+++ app/application/use_cases/create_trip_link_use_case.py
@@ -0,0 +1,37 @@
+import uuid
+from app.application.dto.link_dto import LinkDTO, LinkResponseDTO
+from app.domain.exceptions.trip_not_found_error import TripNotFoundError
+from app.domain.ports.trip_repository import TripRepository
+from app.domain.ports.unit_of_work import UnitOfWork
+from app.domain.entities.link import Link
+
+
+class CreateTripLinkUseCase:
+    def __init__(
+        self,
+        trip_repo: TripRepository,
+        uow: UnitOfWork
+    ) -> None:
+        self.__trip_repo = trip_repo
+        self.__uow = uow
+
+    async def execute(self, trip_id: uuid.UUID, link: LinkDTO) -> LinkResponseDTO:
+        async with self.__uow:
+            trip_found = await self.__trip_repo.get_by_id(trip_id)
+            if not trip_found:
+                raise TripNotFoundError(trip_id)
+
+            new_link = Link(
+                id=uuid.uuid1(),
+                link=link.link,
+                title=link.title
+            )
+
+            trip_found.links = new_link
+            await self.__trip_repo.save(trip_found)
+
+            return LinkResponseDTO(
+                id=new_link.id,
+                link=new_link.link,
+                title=new_link.title
+            )
