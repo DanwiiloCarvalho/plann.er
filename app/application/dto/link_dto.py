23716158f55b20f3d91aeeef65ef2c249caca48d
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-22 02:45:08 +0000

    feat: add LinkResponseDTO

--- app/application/dto/link_dto.py
+++ app/application/dto/link_dto.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass
+import uuid
 
 
 @dataclass
 class LinkDTO:
+    link: str
+    title: str
+
+
+@dataclass
+class LinkResponseDTO:
+    id: uuid.UUID
     link: str
     title: str
