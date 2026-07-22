dac4c6e925ebb62565a2921f86760b33005cfe18
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-22 19:38:01 +0000

    feat: add ActivityResponseDTO for activity response handling

--- app/application/dto/activity_dto.py
+++ app/application/dto/activity_dto.py
@@ -1,9 +1,18 @@
 from dataclasses import dataclass
 from datetime import date, time
+import uuid
 
 
 @dataclass
 class ActivityDTO:
+    title: str
+    date: date
+    time: time
+
+
+@dataclass
+class ActivityResponseDTO:
+    id: uuid.UUID
     title: str
     date: date
     time: time
