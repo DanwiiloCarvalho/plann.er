647e54db731194a2f2c3e7015cb4a68e935e466d
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-07-29 23:15:48 +0000

    feat: add TEST_DATABASE_URL to config

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     APP_HOST: str
     APP_PORT: str
     DATABASE_URL: str | None = None
+    TEST_DATABASE_URL: str | None = None
     API_PREFIX: str | None = None
     OWNER_NAME: str
     EMAIL_USERNAME: str
